fbd7c6c9f433b57d727771784b21c01d383038fb
SladeSouzaSantos <greencity.sustentavel@gmail.com>
2023-12-27 13:43:55 -0300

    Implantação da Estrutura Lista Encadeada Dupla Extremidade.

---
+++ listas_encadeadas/listaEncadeadaDuplaExtremidade.py
@@ -0,0 +1,61 @@
+from listaEncadeadaSimples import ListaEncadeadaSimples
+from no import No
+
+class ListaEncadeadaDuplaExtremidade(ListaEncadeadaSimples):
+    def __init__(self):        
+        super().__init__()
+        self.ultimo = None
+    
+    def insere_inicio(self, valor):
+        '''override'''
+        novo = No(valor)
+        if self.primeiro == None:
+            self.ultimo = novo
+        novo.proximo = self.primeiro
+        self.primeiro = novo
+    
+    def insere_final(self, valor):
+        novo = No(valor)
+        if self.primeiro == None:
+            self.primeiro = novo
+        else: 
+            self.ultimo.proximo = novo
+        self.ultimo = novo
+        
+    def excluir_inicio(self):
+        if self.primeiro == None:
+            print("A lista está vazia")
+            return
+        
+        temp = self.primeiro
+        if self.primeiro.proximo == None:
+            self.ultimo = None
+        self.primeiro = self.primeiro.proximo
+        return temp
+    
+    def excluir_posicao(self, valor):
+        if self.primeiro == None:
+            print('A lista está vazia')
+            return None
+        
+        atual = self.primeiro
+        anterior = self.primeiro
+        while atual.valor != valor:
+            if atual.proximo == None:
+                return None
+            else:
+                anterior = atual
+                atual = atual.proximo
+        
+        
+        if atual == self.primeiro:
+            if atual == self.ultimo:
+                self.ultimo = self.primeiro.proximo           
+            self.primeiro = self.primeiro.proximo        
+        else:
+            anterior.proximo = atual.proximo
+
+        if atual == self.ultimo:
+            self.ultimo = anterior
+        
+        return atual
--- listas_encadeadas/listaEncadeadaSimplesApp.py
+++ listas_encadeadas/listaEncadeadaSimplesApp.py
@@ -1,68 +1,102 @@
 from listaEncadeadaSimples import ListaEncadeadaSimples
+from listaEncadeadaDuplaExtremidade import ListaEncadeadaDuplaExtremidade
 
+def exemploListaEncadeadaSimples():
+  lista = ListaEncadeadaSimples()
+  lista.insere_inicio(1)
+  lista.insere_inicio(2)
+  lista.insere_inicio(3)
+  lista.insere_inicio(4)
+  lista.insere_inicio(5)
+  lista.mostrar()
 
-lista = ListaEncadeadaSimples()
-lista.insere_inicio(1)
-lista.insere_inicio(2)
-lista.insere_inicio(3)
-lista.insere_inicio(4)
-lista.insere_inicio(5)
-lista.mostrar()
+  print("-----------")
 
-print("-----------")
+  lista.excluir_inicio()
+  lista.mostrar()
 
-lista.excluir_inicio()
-lista.mostrar()
+  print("-----------")
 
-print("-----------")
+  lista.excluir_inicio()
+  lista.excluir_inicio()
+  lista.mostrar()
 
-lista.excluir_inicio()
-lista.excluir_inicio()
-lista.mostrar()
+  print("-----------")
 
-print("-----------")
+  lista.excluir_inicio()
+  lista.mostrar()
 
-lista.excluir_inicio()
-lista.mostrar()
+  print("-----------")
 
-print("-----------")
+  lista.excluir_inicio()
+  lista.mostrar()
 
-lista.excluir_inicio()
-lista.mostrar()
+  print("-----------")
 
-print("-----------")
+  lista.insere_inicio(1)
+  lista.insere_inicio(2)
+  lista.insere_inicio(3)
+  lista.insere_inicio(4)
+  lista.insere_inicio(5)
+  lista.mostrar()
 
-lista.insere_inicio(1)
-lista.insere_inicio(2)
-lista.insere_inicio(3)
-lista.insere_inicio(4)
-lista.insere_inicio(5)
-lista.mostrar()
+  print("-----------")
 
-print("-----------")
+  pesquisa = lista.pesquisa(3)
+  if pesquisa != None:
+    print('Encontrado: ', pesquisa.valor)
+  else:
+    print('Não encontrado')
+    
+  print("-----------")
 
-pesquisa = lista.pesquisa(3)
-if pesquisa != None:
-  print('Encontrado: ', pesquisa.valor)
-else:
-  print('Não encontrado')
+  lista.excluir_posicao(3)
+  lista.mostrar()
+
+  print("-----------")
+
+  lista.excluir_posicao(1)
+  lista.mostrar()
+
+  print("-----------")
+
+  lista.excluir_posicao(2)
+  lista.mostrar()
+
+  print("-----------")
+
+  lista.excluir_inicio()
+  lista.mostrar()
   
-print("-----------")
-
-lista.excluir_posicao(3)
-lista.mostrar()
-
-print("-----------")
-
-lista.excluir_posicao(1)
-lista.mostrar()
-
-print("-----------")
-
-lista.excluir_posicao(5)
-lista.mostrar()
-
-print("-----------")
-
-lista.excluir_inicio()
-lista.mostrar()
+def exemploListaEncadeadaDuplaExtremidade():
+  list = ListaEncadeadaDuplaExtremidade()
+  list.insere_inicio(1)
+  print(list.primeiro, list.ultimo)
+  list.insere_inicio(2)
+  list.insere_inicio(3)
+  list.insere_final(4)
+  list.mostrar()
+  print(list.primeiro, list.ultimo)
+  list.excluir_inicio()
+  list.mostrar()
+  print(list.primeiro, list.ultimo)
+  list.insere_final(5)
+  list.mostrar()
+  print(list.primeiro, list.ultimo)
+  list.excluir_posicao(5)
+  list.mostrar()
+  print(list.primeiro, list.ultimo)
+  list.excluir_posicao(4)
+  list.excluir_posicao(2)
+  list.mostrar()
+  print(list.primeiro, list.ultimo)
+  list.excluir_posicao(1)
+  list.mostrar()
+  print(list.primeiro, list.ultimo)
+  list.excluir_inicio()
+  list.excluir_posicao(1)
+  
+print("Lista Encadeada Simples")
+exemploListaEncadeadaSimples()
+print("Lista Encadeada Dupla Extremidade")
+exemploListaEncadeadaDuplaExtremidade()
